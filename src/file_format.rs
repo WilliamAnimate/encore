f88bfabc69ab421805359221cf519a42a4a08e60
WilliamAnimate <nyameowmeow@duck.com>
2024-10-25 22:40:57 -0400

    fix: first playlist entry breaks
    bug breakdown:
    seeks ahead which seems to do something weird with the buffer
    so we seek back to 0

--- src/file_format.rs
+++ src/file_format.rs
@@ -11,10 +11,12 @@ static FILE_HEADERS: &[(&[u8], FileFormat, u64)] = &[
 /// because `file-format is bloated
 /// i did it in 25 SLOC
 pub fn check_file(file: &mut BufReader<File>) -> Result<&FileFormat, Box<dyn std::error::Error>> {
+    use std::io::SeekFrom;
+
     let mut ret: &FileFormat = &FileFormat::Other;
     for (header, format, header_offset) in FILE_HEADERS {
         let mut buf = vec![0; header.len()];
-        if let Err(_) = file.seek(std::io::SeekFrom::Start(*header_offset)) {
+        if let Err(_) = file.seek(SeekFrom::Start(*header_offset)) {
             // possibly out of bounds
             continue;
         }
@@ -26,6 +28,7 @@ pub fn check_file(file: &mut BufReader<File>) -> Result<&FileFormat, Box<dyn std
         ret = format;
         break;
     }
+    file.seek(SeekFrom::Start(0))?;
 
     Ok(ret)
 }
